9eeab8831e7fa59e7dc9ff5989e8d760f96dc78a
Adam <micro.83@hotmail.com>
2019-07-21 12:15:25 +0300

    Update int_float_string_input.php

--- int_float_string_input.php
+++ int_float_string_input.php
@@ -1,24 +1,12 @@
 <?php //php 7.0.8
 
 $input = readline();
-if (ctype_digit(strval($input)))
-    {
+if (ctype_digit(strval($input))) {
     echo "int";
-    }
-else if (is_numeric($input))
-    {
+} elseif (is_numeric($input)) {
     echo "float";
-    }
-else if (is_string($input))
-     {
+} elseif (is_string($input)) {
     echo "string";
-    }
-     var_dump((float)$input);          
-          
-    
-
-
-          
-
-
-?>
+}
+     var_dump((float)$input);
+     
